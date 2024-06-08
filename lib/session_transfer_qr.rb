8ceb9f3488137c8e6d5cda743728177661f5d133
Abhilash Reddy <abhilashmv@outlook.com>
2024-06-08 22:16:24 +0530

    Fix Rails secret key base

--- lib/session_transfer_qr.rb
+++ lib/session_transfer_qr.rb
@@ -4,16 +4,18 @@ require "jwt"
 
 module SessionTransferQR
   class Transfer
-    SECRET_KEY = Rails.application.secret_key_base
+    def self.secret_key
+      Rails.application.secret_key_base
+    end
 
     def self.generate_qr_code(session_data)
-      token = JWT.encode(session_data, SECRET_KEY, 'HS256')
+      token = JWT.encode(session_data, secret_key, 'HS256')
       qrcode = RQRCode::QRCode.new(token)
       qrcode.as_png(size: 300)
     end
 
     def self.decode_qr_code(token)
-      decoded_token = JWT.decode(token, SECRET_KEY, true, { algorithm: 'HS256' })
+      decoded_token = JWT.decode(token, secret_key, true, { algorithm: 'HS256' })
       decoded_token.first
     rescue JWT::DecodeError
       nil
